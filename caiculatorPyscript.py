fe9ff2c483108ba3662ea96d935a0814f9a1c42b
stevenlchow7 <64657835+stevenlchow7@users.noreply.github.com>
2024-11-03 10:01:34 -0600

    Add files via upload

--- caiculatorPyscript.py
+++ caiculatorPyscript.py
@@ -5,13 +5,33 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def calculate(num1, op, num2):
-    with open('model.pkl', 'rb') as f:
+def calculate(value):
+    with open('./model.pkl', 'rb') as f:
         model = pickle.load(f)
+
+    num1, num2, sub, mult, div = evalu(value)
     
-    exp = pd.DataFrame({"Num1": [num1], "Num2": [num2], "sub": [1 if op == "-" else 0], "mult": [1 if op == "*" else 0], "div": [1 if op == "/" else 0]})
+    exp = pd.DataFrame({"Num1": [num1], "Num2": [num2], "sub": [sub], "mult": [mult], "div": [div]})
 
     result = model.predict(exp)
     return result[0]
 
-
+def evalu(value):
+    op = ""
+    sub = 0
+    mult = 0
+    div = 0
+    if "+" in value:
+        op = "+"
+    elif "-" in value:
+        op = "-"
+        sub = 1
+    elif "*" in value:
+        op = "*"
+        mult = 1
+    elif "/" in value:
+        op = "/"
+        div = 1
+    num1 = value[:value.index(op)]
+    num2 = value[value.index(op) + 1:]
+    return num1, num2, sub, mult, div
